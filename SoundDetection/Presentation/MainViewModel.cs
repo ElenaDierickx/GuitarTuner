b6cb60c5f9be04cc10a4281d794db242f0b5ca80
RobbeDierickx <44427819+RobbeDierickx@users.noreply.github.com>
2021-10-05 12:17:08 +0200

    Project setup

---
+++ SoundDetection/Presentation/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LogicLayer;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    public class MainViewModel : ObservableObject
+    {
+        private readonly IMicrophone microphone;
+        public MainViewModel(IMicrophone microphone)
+        {
+            this.microphone = microphone;
+        }
+    }
+}
