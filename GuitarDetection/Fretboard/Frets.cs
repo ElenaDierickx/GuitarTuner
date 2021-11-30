f4c26dff5377983f3e0c7a196aafc9acd6d21da3
RobbeDierickx <44427819+RobbeDierickx@users.noreply.github.com>
2021-11-30 19:36:22 +0100

    Open string detection

---
+++ GuitarDetection/Fretboard/Frets.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Fretboard
+{
+    public class Frets
+    {
+        public static string getFret(double frequency)
+        {
+            if(frequency > 80.41)
+            {
+                if (frequency > 85.31)
+                {
+                    if(frequency > 90.50)
+                    {
+                        if (frequency > 102.83)
+                        {
+                            return "E2";
+                        }
+                        return "E2";
+                    }
+                    return "E1";
+                }
+                return "E0";
+            } else
+            {
+                return "NA";
+            }
+        }
+
+        public static string getString(double frequency)
+        {
+            if(frequency >= 80.41 && frequency <= 84.41)
+            {
+                return "E";
+            }else if (frequency >= 108.0 && frequency <= 112.0)
+            {
+                return "A";
+            }
+            else if (frequency >= 144.83 && frequency <= 148.83)
+            {
+                return "D";
+            }
+            else if (frequency >= 194.00 && frequency <= 198.00)
+            {
+                return "G";
+            }
+            else if (frequency >= 244.94 && frequency <= 248.94)
+            {
+                return "B";
+            }
+            else if (frequency >= 327.63 && frequency <= 331.63)
+            {
+                return "e";
+            }
+            else
+            {
+                return "NA";
+            }
+        }
+    }
+
+}
---
+++ GuitarDetection/GuitarDetection/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Fretboard;
+using LogicLayer;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarDetection
+{
+    
+    public class MainViewModel : ObservableObject
+    {
+        private string fret;
+        public string Fret
+        {
+            get { return fret; }
+            set
+            {
+                if (fret != value)
+                {
+                    fret = value;
+                    OnPropertyChanged("Fret");
+                }
+            }
+        }
+
+        private readonly IMicrophone microphone;
+        public MainViewModel(IMicrophone microphone)
+        {
+            this.microphone = microphone;
+            Task.Run(GetFFT);
+        }
+
+        private void GetFFT()
+        {
+            double freq;
+            while (true)
+            {
+
+                double[] fftArray = microphone.getFrequency();
+                double max = fftArray.Max();
+                if (max != 0)
+                {
+                    int fund_freq = 0;
+                    double[] sum = new double[fftArray.Length / 8];
+                    double max_value2 = max;
+                    for (int k = 0; k < fftArray.Length / 8; k++)
+                    {
+                        sum[k] = fftArray[k] * fftArray[2 * k] * fftArray[3 * k];
+                        // find fundamental frequency (maximum value in plot)
+                        if (sum[k] > max_value2 && k > 0)
+                        {
+                            max_value2 = sum[k];
+                            fund_freq = k;
+                        }
+                    }
+                    freq = fund_freq * 8000 / 16384.0;
+                    Fret = Frets.getString(freq);
+                }
+
+
+            }
+        }
+    }
+}
